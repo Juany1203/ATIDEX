017f09e028eb20c2ed27163a2e79ee68b269dbac
Juany1203 <77596287+Juany1203@users.noreply.github.com>
2021-05-21 23:10:09 -0600

    Add files via upload

--- AtiDex.sql
+++ AtiDex.sql
@@ -1,40 +1,63 @@
 create database AtiDex
 use AtiDex
+drop database Atidex
 
 create table Pokemon
 (
-
-PokemonID int IDENTITY(0,1) PRIMARY KEY,
-Nombre varchar(20) UNIQUE,
-salud int,
-ATespecial varchar(30),
-DFespecial varchar(30),
-ataque int,
-defensa int, 
-velocidad int,
-generacion int,
+PokemonID int IDENTITY(0,1) PRIMARY KEY not null,
+Nombre varchar(20) UNIQUE not null,
+salud int not null,
+ATespecial int not null,
+DFespecial int not null,
+ataque int not null,
+defensa int not null, 
+velocidad int not null,
+generacion int not null,
 legendario bit not null,
 Imagenpokemon image, 
+IDEntrenador int not null,
+
 )
 
+create table TABLA_INTERMEDIA_MovPokemon
+(
+PokemonID int foreign key(PokemonID) references Pokemon(PokemonID),
+IDMovimiento int foreign key (IDMovimiento) references Movimientos (IDMovimiento),
+)
 
 create table Movimientos
 (
-IDMovimiento varchar(6) PRIMARY KEY,
-NOMBREMovimiento varchar(15),
-DescripcionMovimiento varchar(15),
-tipoMovimiento varchar(10),
-PokemonID int,
+IDMovimiento int IDENTITY(0,1) PRIMARY KEY not null,
+NOMBREMovimiento varchar(15) not null,
+DescripcionMovimiento varchar(15) not null,
+tipoMovimiento varchar(10) not null,
+PokemonID int not null,
 )
+
+create table TABLA_INTERMEDIA_TipPokemon
+(
+PokemonID int foreign key(PokemonID) references Pokemon(PokemonID),
+MovID  int foreign key (MovID) references Tipos (MovID),
+)
+
 
 create table Tipos
 (
-MovID Varchar(6) PRIMARY KEY,
-MovNombre Varchar(15),
-PokemonID int,
+MovID int IDENTITY(0,1) PRIMARY KEY not null,
+MovNombre Varchar(15) not null,
 )
-ALTER TABLE Movimientos
-ADD FOREIGN KEY (PokemonID) REFERENCES Pokemon(PokemonID);
 
-ALTER TABLE Tipos
-ADD FOREIGN KEY (PokemonID) REFERENCES Pokemon(PokemonID);
+create table Entrenadores
+(
+IDEntrenador int IDENTITY(0,1) PRIMARY KEY not null,
+provincia varchar(10) not null,
+canton varchar(10) not null,
+distrito varchar(10) not null,
+Exacto varchar(30) not null,
+telefono int,
+correoElectronico varchar(30) not null,
+sitioweb varchar(30) not null, -- faltan las redes sociales
+)
+
+ALTER TABLE Pokemon
+ADD FOREIGN KEY (IDEntrenador) references Entrenadores (IDEntrenador);

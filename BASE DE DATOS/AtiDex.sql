ea3e29e7381a57dc6432b408eb8648236019b11d
Juan Andrés Fernández <juanfdezcamacho@gmail.com>
2021-05-27 18:38:03 -0600

    Update AtiDex.sql
    Se cambiaron las tablas de admin y entrenadores, se juntaron y renombraron a usuario y se agrego un bit para elegir entre los usuarios

--- BASE DE DATOS/AtiDex.sql
+++ BASE DE DATOS/AtiDex.sql
@@ -13,26 +13,69 @@ create database AtiDex
 use AtiDex
 drop database Atidex
 
+
+
+-- Tabla del pokemon generico
+
 create table Pokemon
 (
 PokemonID int IDENTITY(0,1) PRIMARY KEY not null,
 nombrePokemon varchar(20) UNIQUE not null,
-saludPokemon varchar not null,
-ataqueEspecialPokemon varchar not null,
-defensaEspecialPokemon varchar not null,      --El pokemonID no debería de ser (0, 1) en vez de (10, 1)
-ataquePokemon varchar not null,
-defensaPokemon varchar not null, 
-velocidadPokemon varchar not null,
 generacionPokemon varchar not null,
 legendarioPokemon bit not null,
 imagenPokemon image, 
 )
 
+
+-- tabla de todos los usuarios
+
+create table Usuario
+(
+UsuarioID int IDENTITY(0,1) PRIMARY KEY not null,
+UserNameUsuario VARCHAR (10) UNIQUE not null,
+contrasenaUsuario VARCHAR (10) not null,
+TipoUsuario bit not null, -- Esto indica si el ususario es admin o entrenador, 1 si es admin 0 si es entrenador
+nombreUsuario VARCHAR (10) not null,
+apellido1Usuario VARCHAR (10) not null,
+apellido2Usuario VARCHAR (10) not null,
+provinciaUsuario varchar(10) not null,
+cantonUsuario varchar(10) not null,
+distritoUsuario varchar(10) not null,
+direccionUsuario varchar(30) not null,
+telefonoUsuario varchar(9) not null,
+correoElectronicoUsuario varchar(30) not null,
+sitiowebUsuario varchar(30), 
+perfilFBUsuario varchar (10),
+perfilTWUsuario varchar (10),
+perfilIGUsuario varchar (10),
+)
+
+-- tabla que contiene los pokemons por entrenador
+
+
+create Table TrainerPokemon(
+TrainerPokemonID int IDENTITY(0,1) PRIMARY KEY not null,
+EntrenadorIDTrainerPokemon int foreign key(EntrenadorIDTrainerPokemon) references Usuario(UsuarioID),
+PokemonID int foreign key(PokemonID) references Pokemon(PokemonID),
+saludTrainerPokemon varchar not null,
+ataqueEspecialTrainerPokemon varchar not null,
+defensaTrainerEspecialPokemon varchar not null,     
+ataqueTrainerPokemon varchar not null,
+defensaTrainerPokemon varchar not null, 
+velocidadTrainerPokemon varchar not null,
+EstadoPokemon varchar(10) not null,
+)
+
+-- tabla del los tipos de pokemon y movimientos que hay
+
 create table Tipo
 (
-TipoID int IDENTITY(0,1) PRIMARY KEY not null,    --Programar restricción del UNIQUE
+TipoID int IDENTITY(0,1) PRIMARY KEY not null,  
 TipoNombre Varchar(15) UNIQUE not null,
 )
+
+-- Movimientos para asignar
+
 
 create table Movimientos
 (
@@ -42,12 +85,16 @@ descripcionMovimiento varchar(15) not null,
 TipoID int foreign key (TipoID) references Tipo(TipoID),
 )
 
---Relaciona tabla Pokemon con tabla movimientos
+
+--Relaciona tabla Pokemon por entrenador con tabla movimientos
 create table TABLA_INTERMEDIA_MovPokemon
 (
-PokemonID int foreign key(PokemonID) references Pokemon(PokemonID),
-IDMovimiento int foreign key (IDMovimiento) references Movimientos(MovimientoID),
+TrainerPokemon_IntermediaID int foreign key(TrainerPokemon_IntermediaID) references TrainerPokemon(TrainerPokemonID),
+Movimiento_IntermediaID int foreign key (Movimiento_IntermediaID) references Movimientos(MovimientoID),
 )
+
+--tabla intermedia entre los pokemones genericos y los tipos
+
 
 create table intPOKETIPO
 (
@@ -55,56 +102,13 @@ PokemonID int foreign key(PokemonID) references Pokemon(PokemonID),
 TipoID int foreign key (TipoID) references Tipo(TipoID),
 )
 
-create table Admin 
-(
-AdminID int IDENTITY (0,1) PRIMARY KEY not null,
-userNameAdmin VARCHAR (10) UNIQUE not null,
-contrasenaAdmin VARCHAR (10) not null,
-nombreAdmin VARCHAR (10) not null,
-apellido1Admin VARCHAR (10) not null,
-apellido2Admin VARCHAR (10) not null,
-provinciaAdminr varchar(10) not null,
-cantonAdmin varchar(10) not null,
-distritoAdmin varchar(10) not null,
-direccionAdmin varchar(30) not null,
-telefonoAdmin varchar(9) not null,
-correoElectronicoAdmin varchar(30) not null,
-)
-
-create table Entrenadores
-(
-EntrenadorID int IDENTITY(0,1) PRIMARY KEY not null,
-UserNameEntrenador VARCHAR (10) UNIQUE not null,
-contrasenaEntrenador VARCHAR (10) not null,
-nombreEntrenador VARCHAR (10) not null,
-apellido1Entrenador VARCHAR (10) not null,
-apellido2Entrenador VARCHAR (10) not null,
-provinciaEntrenador varchar(10) not null,
-cantonEntrenador varchar(10) not null,
-distritoEntrenador varchar(10) not null,
-direccionEntrenador varchar(30) not null,
-telefonoEntrenador varchar(9) not null,
-correoElectronicoEntrenador varchar(30) not null,
-sitiowebEntrenador varchar(30), -- le quité el not null, qué pasa si el usuario no tiene uno? si mamon 
-perfilFBEntrenador varchar (10),
-perfilTWEntrenador varchar (10),
-perfilIGEntrenador varchar (10),
-)
-
+-- tabla de las bitacoras para los entrenadores
 
 create table Bitacora
 (
 ID_Bitacora int IDENTITY (0, 1) PRIMARY KEY not null,
 fecha date,
-EntrenadorID int foreign key (EntrenadorID) references Entrenadores (EntrenadorID),
+UserIDBitacora int foreign key (UserIDBitacora) references Usuario (UsuarioID),
 )
-
-
-/* NOTAS IMPORTANTES
-Todos los int's como cedula, ID's, telefonos, etc, los cambiamos para varchar, ya que a la hora de manipular, es más fácil manipular varchar's
-
-*/
-
---QUERY's
 
 

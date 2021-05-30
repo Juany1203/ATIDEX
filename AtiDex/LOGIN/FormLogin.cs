31915636a2e35acd88261c4d07fa2e95898ce0d2
Juan Andrés Fernández <juanfdezcamacho@gmail.com>
2021-05-30 14:16:01 -0600

    LOGIN
    Se agrega el form de LOGIN

---
+++ AtiDex/LOGIN/FormLogin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Atidex
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxPASS_MouseEnter(object sender, EventArgs e)
+        {
+            TextBoxPASS.UseSystemPasswordChar = true;
+        }
+    }
+}
--- AtiDex/Program.cs
+++ AtiDex/Program.cs
@@ -17,7 +17,7 @@ namespace Atidex
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new InterfazDelVisitante());
+            Application.Run(new FormLogin());
 
         }
     }

947e96126fd3e7a3899e5953c7018314275f620a
Juan Andrés Fernández <juanfdezcamacho@gmail.com>
2021-06-01 22:31:14 -0600

    Stored PRocedures
    Se agregaron los procedimientos de Pokemon

---
+++ BASE DE DATOS/storePokemon.sql
@@ -0,0 +1,31 @@
+-- stored procedure para insertar Pokemon
+
+create proc InsertarPokemon
+@nombrePokemon VARCHAR (50),
+@generacionPokemon VARCHAR (50),
+@legendarioPokemon bit,
+@imagenPokemon image
+as
+insert into Pokemon values (@nombrePokemon, @generacionPokemon, @legendarioPokemon, @imagenPokemon)
+go
+
+-- stored procedure para editar Pokemon
+create proc EditarPokemon
+@PokemonID int,
+@nombrePokemon VARCHAR (50),
+@generacionPokemon VARCHAR (50),
+@legendarioPokemon bit,
+@imagenPokemon image
+
+as Update Pokemon set nombrePokemon = @nombrePokemon, generacionPokemon
+= @generacionPokemon, legendarioPokemon = @legendarioPokemon, imagenPokemon = @imagenPokemon
+where PokemonID = @PokemonID
+go
+
+--stored procedure para eliminar Pokemon
+
+create proc EliminarPokemon
+@PokemonIDEliminar int
+as
+delete from Pokemon where PokemonID = @PokemonIDEliminar
+go
